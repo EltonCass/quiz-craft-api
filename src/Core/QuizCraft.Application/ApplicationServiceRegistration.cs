38da5a9e878e74027651c3c6b4ed53ee7f3cc193
elton.cassas <elcassastn50@gmail.com>
2023-08-06 00:52:04 -0400

    refactor scaffolding

---
+++ src/Core/QuizCraft.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using QuizCraft.Application.CategoryManagement;
+using QuizCraft.Application.QuizManagement;
+using System.Reflection;
+
+namespace QuizCraft.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IQuizRepository, QuizRepository>();
+            return services;
+        }
+    }
+}
---
+++ src/Core/QuizCraft.Application/CategoryManagement/CategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) 2023 Elton Cassas. All rights reserved.
+// See LICENSE.txt
+
+using QuizCraft.Models.Entities;
+
+namespace QuizCraft.Application.CategoryManagement;
+
+public class CategoryRepository : ICategoryRepository
+{
+    public Task<Quiz> CreateCategory(Quiz newQuiz, CancellationToken cancellationToken) => throw new NotImplementedException();
+    public Task<Quiz> DeleteCategory(int id, CancellationToken cancellationToken) => throw new NotImplementedException();
+    public Task<IEnumerable<Quiz>> RetrieveCategories(CancellationToken cancellationToken) => throw new NotImplementedException();
+    public Task<Quiz> RetrieveCategory(int id, CancellationToken cancellationToken) => throw new NotImplementedException();
+    public Task<Quiz> UpdateCategory(Quiz quiz, CancellationToken cancellationToken) => throw new NotImplementedException();
+}
